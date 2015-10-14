68a020cebc86b32a9eb5d75bd4a796cf73359cf3
KorsunPavel <pavel.korsun1@gmail.com>
2015-10-14 23:46:54 +0300

    final commit

--- TestIndicatorsAutomation/TTFTopBarPages/TTFHomePage.cs
+++ TestIndicatorsAutomation/TTFTopBarPages/TTFHomePage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -16,6 +16,26 @@ namespace TestIndicatorsAutomation
 {
     public class TTFHomePage : TTFLoginPage
     {
+
+        public void LanguageChecker()
+        {
+        	
+            TTFDriver.myManager.ActiveBrowser.Find.ByAttributes<HtmlAnchor>("href=~MySettings").Click();
+            if (TTFDriver.myManager.ActiveBrowser.Find.ByAttributes<HtmlOption>("selected=selected").Text.Contains("English"))
+                CheckUserName();
+            TTFDriver.myManager.ActiveBrowser.Find.ByTagIndex<HtmlSelect>("select", 0).SelectByText("English");
+
+
+            TTFDriver.myManager.ActiveBrowser.GoBack();
+        }
+
+        public static void CheckUserName()
+        {
+            Assert.IsTrue(
+                TTFDriver.myManager.ActiveBrowser.Find.ByTagIndex<HtmlContainerControl>("fieldset", 0)
+                .Find.ByTagIndex<HtmlDiv>("div", 1).TextContent.Contains(UserName)
+                , "account name is wrong logging failed");
+        }
         // three methods are inherited derived from base class: GoToItem , IsAt , GoToByUrl
     }
 }
